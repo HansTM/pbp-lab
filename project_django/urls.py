59d9b5f3d4d38f30d708b2cd2dadb307fd732591
Hans TM <112265332+HansTM@users.noreply.github.com>
2022-09-29 15:46:34 +0700

    Tambah autoreload

--- project_django/urls.py
+++ project_django/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("__reload__/", include("django_browser_reload.urls")),
     path('wishlist/', include('wishlist.urls')),
     path('', include('wishlist.urls')),
 ]
